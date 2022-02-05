81cd63ac1f764303e06b180c0783a422b485fd89
Jason Day <jason.day.blue@gmail.com>
2022-02-04 21:58:33 -0600

    save

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,8 +1,10 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const WebpackPwaManifest = require("webpack-pwa-manifest");
 const path = require("path");
+const WorkboxPlugin = require("workbox-webpack-plugin");
 const { InjectManifest } = require("workbox-webpack-plugin");
 const { GenerateSW } = require("workbox-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
 // TODO: Add CSS loaders and babel to webpack.
@@ -28,6 +30,9 @@ module.exports = () => {
 				swDest: "service-worker.js",
 			}),
 			new GenerateSW(),
+			new WorkboxPlugin.GenerateSW(),
+			new MiniCssExtractPlugin(),
+
 			new WebpackPwaManifest({
 				name: "TODOs",
 				short_name: "TODOs",
@@ -55,7 +60,31 @@ module.exports = () => {
 		],
 
 		module: {
-			rules: [],
+			rules: [
+				{
+					test: /\.css$/i,
+					use: [
+						MiniCssExtractPlugin.loader,
+						"css-loader",
+					],
+				},
+				{
+					test: /\.(png|svg|jpg|jpeg|gif)$/i,
+					type: "asset/resource",
+				},
+				{
+					test: /\.m?js$/,
+					exclude: /(node_modules|bower_components)/,
+					use: {
+						loader: "babel-loader",
+						options: {
+							presets: [
+								"@babel/preset-env",
+							],
+						},
+					},
+				},
+			],
 		},
 	};
 };
